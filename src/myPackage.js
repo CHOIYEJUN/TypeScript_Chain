76724556059dd9ab6e4e01e744e92c20b22f7816
최예준 <dpwns108@gmail.com>
2024-01-01 13:49:10 +0900

    블록체인 구현하여 커밋함.

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,23 @@
+// @ts-check
+
+/** *
+ * Init the Project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ *
+ */
 export function init(config){
     return true;
 }
 
+
+/** *
+ * exit program
+ * @param {number} code
+ * @returns {number}
+ */
 export function exit(code) {
     return code + 1;
 }
